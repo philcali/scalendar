ed408d8d089f6df044d56cce7ba1d1a9082a8c77
Philip Cali <philip.cali@gmail.com>
2011-02-26 20:48:35 -0600

    Added scala tools publishing.

--- project/build/calendar.scala
+++ project/build/calendar.scala
@@ -2,4 +2,10 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
+
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at 
+                  "http://nexus.scala-tools.org/content/repositories/releases/"
+ 
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)   
 }
