5631932a1cc82cdc033be11e7f6149c4b61b9545
Philip Cali <philip.cali@gmail.com>
2011-05-16 09:58:37 -0500

    ALL the scalatest versions please.

--- project/build/calendar.scala
+++ project/build/calendar.scala
@@ -1,7 +1,15 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  def scalatestVersion = buildScalaVersion match {
+    case v if v contains "2.9" => 
+      "org.scalatest" % "scalatest_%s".format(v) % "1.4.1"
+    case v if v contains "2.8" => 
+      "org.scalatest" % "scalatest" % "1.3"
+    case _ => "org.scalatest" % "scalatest" % "1.1"
+  }
+
+  val scalatest = scalatestVersion % "test" 
 
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at 
